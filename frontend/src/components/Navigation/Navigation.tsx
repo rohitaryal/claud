a1c048eca27896dbe74550111610e11029f3fcda
rohitaryal <erucix@gmail.com>
2025-10-27 06:47:26 +0530

    feat: add navigation component

--- frontend/src/components/Navigation/Navigation.tsx
+++ frontend/src/components/Navigation/Navigation.tsx
@@ -1,1 +1,20 @@
-const Navigation = function () {}
+import LoginButton from '../LoginButton/LoginButton'
+import styles from './Navigation.module.css'
+
+const Navigation = function () {
+    return (
+        <nav className={styles.navContainer}>
+            {/*Asset is in /public*/}
+            <img src="/cloud-original.png" className={styles.logo} />
+            <div className={styles.navLinksContainer}>
+                <a href="#">Join Claud</a>
+                <a href="#">Documentation</a>
+                <a href="#">About Us</a>
+                <a href="#">Terms & Conditions</a>
+            </div>
+            <LoginButton />
+        </nav>
+    )
+}
+
+export default Navigation
