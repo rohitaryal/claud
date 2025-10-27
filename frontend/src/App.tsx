772c1aad321d4e7fd856eae39c9b0fac36d3bd4c
rohitaryal <erucix@gmail.com>
2025-10-27 08:29:54 +0530

    fix: smth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Navigation from './components/Navigation/Navigation'
+import Welcome from './components/Welcome/Welcome'
 
 const App = function () {
     return (
         <>
             <Navigation />
+            <Welcome />
         </>
     )
 }
