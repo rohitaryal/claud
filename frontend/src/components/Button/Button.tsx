a51fcf0f3a5ad27ff3934259fb5e1fe0607ee8d3
rohitaryal <erucix@gmail.com>
2025-10-27 08:27:06 +0530

    fix: make importable

---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import { IoArrowForwardOutline } from 'react-icons/io5'
+import styles from './Button.module.css'
+
+interface ButtonProps {
+  text: string
+  colored: boolean
+}
+
+const Button = function ({ text, colored }: ButtonProps) {
+  return (
+    <button className={styles.button + " " + (colored ? styles.blueButton : styles.whiteButton)}>
+      <span className={styles.text}>{text}</span>
+      <IoArrowForwardOutline className={styles.arrow} color={colored ? 'var(--blue-dark)' : 'white'} size={40} />
+    </button>
+  )
+}
+
+export default Button
