4a579a856d8e36c093c000a7a7cd213d26265276
rohitaryal <erucix@gmail.com>
2025-10-27 09:59:17 +0530

    fix: smth

--- frontend/src/pages/Welcome/Welcome.tsx
+++ frontend/src/pages/Welcome/Welcome.tsx
@@ -1,25 +1,30 @@
-import Button from '../Button/Button'
+import Button from '../../components/Button/Button'
+import Footer from '../../components/Footer/Footer'
+import Navigation from '../../components/Navigation/Navigation'
 import styles from './Welcome.module.css'
 
 const Welcome = function () {
     return (
-        <div className={styles.welcomeContainer}>
-            <div className={styles.welcomeText}>
-                <span className={styles.firstLine}>Save Your Secrets</span>
-                <span className={styles.secondLine}>With Confidence</span>
-                <span className={styles.finalLine}>
-                    One platform to instantly <span>store</span>, <span>sync</span> and{' '}
-                    <span>share</span> files
-                </span>
-            </div>
+        <>
+            <Navigation />
+            <div className={styles.welcomeContainer}>
+                <div className={styles.welcomeText}>
+                    <span className={styles.firstLine}>Save Your Secrets</span>
+                    <span className={styles.secondLine}>With Confidence</span>
+                    <span className={styles.finalLine}>
+                        One platform to instantly <span>store</span>, <span>sync</span> and{' '}
+                        <span>share</span> files
+                    </span>
+                </div>
 
-            <div className={styles.buttonsContainer}>
-                <Button text="Log In" colored={false} />
-                <Button text="Sign Up" colored={true} />
+                <div className={styles.buttonsContainer}>
+                    <Button text="Log In" colored={false} />
+                    <Button text="Sign Up" colored={true} />
+                </div>
+                <img src="/people.jpg" className={styles.people} />
             </div>
-
-            <img src="/people.jpg" className={styles.people} />
-        </div>
+            <Footer />
+        </>
     )
 }
 
