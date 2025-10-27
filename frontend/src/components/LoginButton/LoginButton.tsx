cc6b24e799d88b7b3910c789a022be13d7ac0f5d
rohitaryal <erucix@gmail.com>
2025-10-27 06:49:00 +0530

    fix: formatting

--- frontend/src/components/LoginButton/LoginButton.tsx
+++ frontend/src/components/LoginButton/LoginButton.tsx
@@ -2,12 +2,12 @@ import { IoArrowForwardOutline } from 'react-icons/io5'
 import styles from './LoginButton.module.css'
 
 const LoginButton = function () {
-  return (
-    <button className={styles.loginButton}>
-      <span className={styles.loginText}>Login to Claud</span>
-      <IoArrowForwardOutline className={styles.arrow} size={40} />
-    </button>
-  )
+    return (
+        <button className={styles.loginButton}>
+            <span className={styles.loginText}>Login to Claud</span>
+            <IoArrowForwardOutline className={styles.arrow} size={40} />
+        </button>
+    )
 }
 
 export default LoginButton
