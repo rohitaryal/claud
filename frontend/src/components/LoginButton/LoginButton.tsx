912895d5887977f6367ccac90d57611caa536392
rohitaryal <erucix@gmail.com>
2025-10-27 06:47:04 +0530

    feat: add login button component

---
+++ frontend/src/components/LoginButton/LoginButton.tsx
@@ -0,0 +1,13 @@
+import { IoArrowForwardOutline } from 'react-icons/io5'
+import styles from './LoginButton.module.css'
+
+const LoginButton = function () {
+  return (
+    <button className={styles.loginButton}>
+      <span className={styles.loginText}>Login to Claud</span>
+      <IoArrowForwardOutline className={styles.arrow} size={40} />
+    </button>
+  )
+}
+
+export default LoginButton
