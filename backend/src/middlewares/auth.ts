fdcda529a9702c0f9a481c08cbe60127321ef8eb
rohitaryal <erucix@gmail.com>
2025-10-31 05:16:44 +0530

    feat: skeleton auth middleware

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -1,9 +1,21 @@
 import { createMiddleware } from 'hono/factory'
+import { deocdeCookie } from '../utils/cookie'
 
-const checkAuth = createMiddleware(async (c, next) => {
-    const cookies = c.req.header('Cookie')
+export const checkAuth = createMiddleware(async (c, next) => {
+  const cookies = c.req.header('Cookie')
 
-    if (!cookies?.trim()) {
-        return c.redirect('/login')
-    }
-})
+  if (!cookies?.trim()) {
+    return c.redirect('/login')
+  }
+
+  const parsedCookie = deocdeCookie(cookies);
+  if (!parsedCookie) {
+    return c.redirect('/login');
+  }
+
+  if (!parsedCookie.trackingID || !parsedCookie.session || !parsedCookie.username) {
+    return c.redirect('/login');
+  }
+
+  return await next();
+});
